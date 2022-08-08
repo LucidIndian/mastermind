b21df6cd5d786d6b035a75422cf32a8e08c6898b
Tygh Walters <tyghwalters@gmail.com>
2022-08-07 18:17:59 -0700

    Trying to make a different array for each round to preserve the history of the guesses

--- mastermind.rb
+++ mastermind.rb
@@ -4,17 +4,9 @@ class CodeMaker
 
   def initialize(name)
     @name = name
-    @code = 0000
-    puts "the code is now #{code} upon initialize"
     puts "the code maker is #{name}!"
   end
   
-  def make_code
-    puts "#{name} is making the code..." 
-    sleep 1
-    @code = 4321
-    puts "the code is now #{code} upon make_code method"
-  end
 end
 
 class CodeBreaker
@@ -34,19 +26,60 @@ class CodeBreaker
   end
 end
 
-class Mastermind 
+class Mastermind
 
-  attr_accessor :codepegs, :keypegs
+  attr_accessor :codepegs1, :codepegs2, :codepegs3, 
+  :codepegs4, :codepegs5, :codepegs6, 
+  :codepegs7, :codepegs8, :codepegs9,
+  :codepegs10, :codepegs11, :codepegs12,
+  :keypegs1, :keypegs2, :keypegs3, 
+  :keypegs4, :keypegs5, :keypegs6, 
+  :keypegs7, :keypegs8, :keypegs9,
+  :keypegs10, :keypegs11, :keypegs12,
+  :winner, :round, :code, :player, :cpu
 
   def initialize(player, cpu) # passing in objects
-    @codepegs = [nil, nil, nil, nil]
-    @keypegs = [nil, nil, nil, nil]
+    @codepegs1 = [nil, nil, nil, nil]
+    @codepegs2 = [nil, nil, nil, nil]
+    @codepegs3 = [nil, nil, nil, nil]
+    @codepegs4 = [nil, nil, nil, nil]
+    @codepegs5 = [nil, nil, nil, nil]
+    @codepegs6 = [nil, nil, nil, nil]
+    @codepegs7 = [nil, nil, nil, nil]
+    @codepegs8 = [nil, nil, nil, nil]
+    @codepegs9 = [nil, nil, nil, nil]
+    @codepegs10 = [nil, nil, nil, nil]
+    @codepegs11 = [nil, nil, nil, nil]
+    @codepegs12 = [nil, nil, nil, nil]
+    @keypegs1 = [nil, nil, nil, nil]
+    @keypegs2 = [nil, nil, nil, nil]
+    @keypegs3 = [nil, nil, nil, nil]
+    @keypegs4 = [nil, nil, nil, nil]
+    @keypegs5 = [nil, nil, nil, nil]
+    @keypegs6 = [nil, nil, nil, nil]
+    @keypegs7 = [nil, nil, nil, nil]
+    @keypegs8 = [nil, nil, nil, nil]
+    @keypegs9 = [nil, nil, nil, nil]
+    @keypegs10 = [nil, nil, nil, nil]
+    @keypegs11 = [nil, nil, nil, nil]
+    @keypegs12 = [nil, nil, nil, nil]
     @player = player
     @cpu = cpu
+    @winner = false
+    @round = 0
+    @code = nil
   end
   
+  def make_code
+    puts "#{cpu} is making the code..." 
+    sleep 2
+    @code = 4321
+    puts "the code is now #{code} at the make_code method"
+    show_board
+  end
+
   def change_board(code_guess)
-    self.codepegs = code_guess.to_s.split('').map(&:to_i)
+    self.codepegs1 = code_guess.to_s.split('').map(&:to_i)
     show_board
   end
 
@@ -54,11 +87,11 @@ class Mastermind
   def show_board
     puts "
     ________________________
-    1- #{@codepegs[0]}-#{@codepegs[1]}-#{@codepegs[2]}-#{@codepegs[3]}-[][][][]
+    01- #{@codepegs1[0]}-#{@codepegs1[1]}-#{@codepegs1[2]}-#{@codepegs1[3]}-[][][][]
     ________________________
-    02- _________________
+    02- #{@codepegs2[0]}-#{@codepegs2[1]}-#{@codepegs2[2]}-#{@codepegs2[3]}-[][][][]
     ________________________
-    03- _________________
+    03- #{@codepegs3[0]}-#{@codepegs3[1]}-#{@codepegs3[2]}-#{@codepegs3[3]}-[][][][]
     ________________________
     04- _________________
     05- _________________
@@ -70,6 +103,20 @@ class Mastermind
     11- _________________
     12- _________________
     "
+
+    winner_check
+
+  end
+
+  def winner_check
+    if code == codepegs1
+    winner = true
+    else
+     # nada
+    end
+    # increment round since now winner
+    # write code to end the game if round = 12
+    self.round += 1
   end
 
 end # class end
@@ -77,10 +124,10 @@ end # class end
 player = CodeBreaker.new("Tygh")
 cpu = CodeMaker.new("Tygh's Laptop")
 sleep 1
-cpu.make_code
 newGame = Mastermind.new(player, cpu)
 newGame.show_board
-newGame.change_board(player.break_code())
+current_guess = player.break_code
+newGame.change_board(current_guess)
 
 # Classes: Player, Computer, Mastermind
 
