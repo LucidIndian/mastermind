a52075fbac03a0f6f6fb71350ecdcd25c9344ba5
Tygh Walters <tyghwalters@gmail.com>
2022-09-14 21:22:10 -0700

    Got keypegs printing to the board
    Still need to write logic to match key pegs with # of code pegs

--- mastermind.rb
+++ mastermind.rb
@@ -39,30 +39,30 @@ class Mastermind
   :winner, :round, :code, :player, :cpu
 
   def initialize(player, cpu, code) # passing in player and cpu objects
-    @codepegs1 = [nil, nil, nil, nil]
-    @codepegs2 = [nil, nil, nil, nil]
-    @codepegs3 = [nil, nil, nil, nil]
-    @codepegs4 = [nil, nil, nil, nil]
-    @codepegs5 = [nil, nil, nil, nil]
-    @codepegs6 = [nil, nil, nil, nil]
-    @codepegs7 = [nil, nil, nil, nil]
-    @codepegs8 = [nil, nil, nil, nil]
-    @codepegs9 = [nil, nil, nil, nil]
-    @codepegs10 = [nil, nil, nil, nil]
-    @codepegs11 = [nil, nil, nil, nil]
-    @codepegs12 = [nil, nil, nil, nil]
-    @keypegs1 = [nil, nil, nil, nil]
-    @keypegs2 = [nil, nil, nil, nil]
-    @keypegs3 = [nil, nil, nil, nil]
-    @keypegs4 = [nil, nil, nil, nil]
-    @keypegs5 = [nil, nil, nil, nil]
-    @keypegs6 = [nil, nil, nil, nil]
-    @keypegs7 = [nil, nil, nil, nil]
-    @keypegs8 = [nil, nil, nil, nil]
-    @keypegs9 = [nil, nil, nil, nil]
-    @keypegs10 = [nil, nil, nil, nil]
-    @keypegs11 = [nil, nil, nil, nil]
-    @keypegs12 = [nil, nil, nil, nil]
+    @codepegs1 = []
+    @codepegs2 = []
+    @codepegs3 = []
+    @codepegs4 = []
+    @codepegs5 = []
+    @codepegs6 = []
+    @codepegs7 = []
+    @codepegs8 = []
+    @codepegs9 = []
+    @codepegs10 = []
+    @codepegs11 = []
+    @codepegs12 = []
+    @keypegs1 = []
+    @keypegs2 = []
+    @keypegs3 = []
+    @keypegs4 = []
+    @keypegs5 = []
+    @keypegs6 = []
+    @keypegs7 = []
+    @keypegs8 = []
+    @keypegs9 = []
+    @keypegs10 = []
+    @keypegs11 = []
+    @keypegs12 = []
     @player = player
     @cpu = cpu
     @code = code
@@ -70,32 +70,46 @@ class Mastermind
     @round = 1 # To increment the guess in each row
   end
 
+ 
   def change_board(code_guess = player.break_code) # calling the break code method and using it in the change board method
+    pegs = keypeg_calc(code_guess)
     case round
     when 1
-      self.codepegs1 = code_guess.to_s.split('').map(&:to_i)
+      self.codepegs1 = code_guess
+      self.keypegs1 = pegs
     when 2
-      self.codepegs2 = code_guess.to_s.split('').map(&:to_i)
+      self.codepegs2 = code_guess
+      self.keypegs2 = pegs
     when 3
-      self.codepegs3 = code_guess.to_s.split('').map(&:to_i)
+      self.codepegs3 = code_guess
+      self.keypegs3 = pegs
     when 4
-      self.codepegs4 = code_guess.to_s.split('').map(&:to_i)
+      self.codepegs4 = code_guess
+      self.keypegs4 = pegs
     when 5
-      self.codepegs5 = code_guess.to_s.split('').map(&:to_i)
+      self.codepegs5 = code_guess
+      self.keypegs5 = pegs
     when 6
-      self.codepegs6 = code_guess.to_s.split('').map(&:to_i)
+      self.codepegs6 = code_guess
+      self.keypegs6 = pegs
     when 7
-      self.codepegs7 = code_guess.to_s.split('').map(&:to_i)
+      self.codepegs7 = code_guess
+      self.keypegs7 = pegs
     when 8
-      self.codepegs8 = code_guess.to_s.split('').map(&:to_i)
+      self.codepegs8 = code_guess
+      self.keypegs8 = pegs
     when 9
-      self.codepegs9 = code_guess.to_s.split('').map(&:to_i)
+      self.codepegs9 = code_guess
+      self.keypegs9 = pegs
     when 10
-      self.codepegs10 = code_guess.to_s.split('').map(&:to_i)
+      self.codepegs10 = code_guess
+      self.keypegs10 = pegs
     when 11
-      self.codepegs11 = code_guess.to_s.split('').map(&:to_i)
+      self.codepegs11 = code_guess
+      self.keypegs11 = pegs
     when 12
-      self.codepegs12 = code_guess.to_s.split('').map(&:to_i)
+      self.codepegs12 = code_guess
+      self.keypegs12 = pegs
     else
       puts "Something wrong with the `round` variable"
     end
@@ -104,35 +118,22 @@ class Mastermind
   end
 
   def show_board
-    puts "________________________
-    01- #{@codepegs1[0]}-#{@codepegs1[1]}-#{@codepegs1[2]}-#{@codepegs1[3]}-[][][][]
-    ________________________
-    02- #{@codepegs2[0]}-#{@codepegs2[1]}-#{@codepegs2[2]}-#{@codepegs2[3]}-[][][][]
-    ________________________
-    03- #{@codepegs3[0]}-#{@codepegs3[1]}-#{@codepegs3[2]}-#{@codepegs3[3]}-[][][][]
-    ________________________
-    04- #{@codepegs4[0]}-#{@codepegs4[1]}-#{@codepegs4[2]}-#{@codepegs4[3]}-[][][][]
-    ________________________
-    05- #{@codepegs5[0]}-#{@codepegs5[1]}-#{@codepegs5[2]}-#{@codepegs5[3]}-[][][][]
-    ________________________
-    06- #{@codepegs6[0]}-#{@codepegs6[1]}-#{@codepegs6[2]}-#{@codepegs6[3]}-[][][][]
-    ________________________
-    07- #{@codepegs7[0]}-#{@codepegs7[1]}-#{@codepegs7[2]}-#{@codepegs7[3]}-[][][][]
-    ________________________
-    08- #{@codepegs8[0]}-#{@codepegs8[1]}-#{@codepegs8[2]}-#{@codepegs8[3]}-[][][][]
-    ________________________
-    09- #{@codepegs9[0]}-#{@codepegs9[1]}-#{@codepegs9[2]}-#{@codepegs9[3]}-[][][][]
-    ________________________
-    10- #{@codepegs10[0]}-#{@codepegs10[1]}-#{@codepegs10[2]}-#{@codepegs10[3]}-[][][][]
-    ________________________
-    11- #{@codepegs11[0]}-#{@codepegs11[1]}-#{@codepegs11[2]}-#{@codepegs11[3]}-[][][][]
-    ________________________
-    12- #{@codepegs12[0]}-#{@codepegs12[1]}-#{@codepegs12[2]}-#{@codepegs12[3]}-[][][][]
-    "
+    puts "--  Code  -  Keys"
+    puts "01- #{@codepegs1} - #{@keypegs1}"
+    puts "02- #{@codepegs2} - #{@keypegs2}"
+    puts "03- #{@codepegs3} - #{@keypegs3}"
+    puts "04- #{@codepegs4} - #{@keypegs4}"
+    puts "05- #{@codepegs5} - #{@keypegs5}"
+    puts "06- #{@codepegs6} - #{@keypegs6}"
+    puts "07- #{@codepegs7} - #{@keypegs7}"
+    puts "08- #{@codepegs8} - #{@keypegs8}"
+    puts "09- #{@codepegs9} - #{@keypegs9}"
+    puts "10- #{@codepegs10} - #{@keypegs10}"
+    puts "11- #{@codepegs11} - #{@keypegs11}"
+    puts "12- #{@codepegs12} - #{@keypegs12}"
   end
 
   def winner_check(code_guess)
-    puts "Winner checking running..."
     if @code == code_guess
       winner = true
       puts "Correct! The Code Breaker Wins!!!"
@@ -148,6 +149,34 @@ class Mastermind
     end
     change_board # keep playing!
   end
+
+  def keypeg_calc(code_guess) # fxn to change the keypegs from the guess so it prints
+    # compare code_guess to @code then determine the right keypeg sequence
+    code_guess_array = code_guess.to_s.split('').map(&:to_i)
+    code_array = @code.to_s.split('').map(&:to_i)
+    puts "code_guess_array is #{code_guess_array}"
+    puts "code_array is #{code_array}"
+    # if the first # of code_guess_array matches the first # of code_array, then print "!" = correct # and order!
+    # elsif the first # of code_guess_array mathes any other # in code_array, then print "X" = correct # but wrong order
+    # else then print "O" = miss
+    # repeat for all
+      # NOTE: If there are duplicate colors in the guess, 
+      # they cannot all be awarded a key peg unless 
+      # they correspond to the same number of duplicate colors in the hidden code. 
+    keypeg_array = code_guess_array.map.with_index do |number, index|
+      if number == code_array[index]
+        "!"
+      elsif code_array.include? code_guess_array[index]
+        "X"
+      else
+        "O"
+      end
+    end
+    puts "keypeg_array is #{keypeg_array}"
+    keypeg_array.join("")
+  end
+
+
 end # class end
 
 # GAME START SEQUENCE
