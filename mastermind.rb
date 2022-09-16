49fd72b4b3783f2e2c938084b2a9cd5a8d78e1a2
Tygh Walters <tyghwalters@gmail.com>
2022-09-15 21:46:35 -0700

    Clean up test statements and display hoard after first guess

--- mastermind.rb
+++ mastermind.rb
@@ -6,9 +6,9 @@ class CodeMaker
   end
   def make_code
     puts "#{self.name} is making the code..." 
-    sleep 2
+    sleep 1
     @code = 4421
-    puts "the code is now #{@code} via the make_code method"
+    puts "The code is now #{@code} via the make_code method" # test
     @code # so it returns the code value
   end
 end
@@ -17,7 +17,7 @@ class CodeBreaker
   attr_accessor :name
   def initialize(name)
     @name = name
-    puts "The code breaker is #{name}!"
+    puts "The Code Breaker is #{name}!"
   end
   def break_code
     puts "Code Breaker, enter your guess"
@@ -70,7 +70,6 @@ class Mastermind
     @round = 1 # To increment the guess in each row
   end
 
- 
   def change_board(code_guess = player.break_code) # calling the break code method and using it in the change board method
     pegs = keypeg_calc(code_guess)
     case round
@@ -118,7 +117,7 @@ class Mastermind
   end
 
   def show_board
-    puts "-- Code -  Keys"
+    puts "-- Code - Keypegs (! = exact, X = inexact, 0 = miss)"
     puts "01- #{@codepegs1} - #{@keypegs1}"
     puts "02- #{@codepegs2} - #{@keypegs2}"
     puts "03- #{@codepegs3} - #{@keypegs3}"
@@ -144,82 +143,61 @@ class Mastermind
     # increment round since no winner
     self.round += 1
     while round == 13 do
-      puts "GAME OVER, The Code Maker Wins!!!" # sensing no more gusses left
+      puts "GAME OVER, The Code Maker Wins!!!" # sensing no more guesses left
       return # break loop
     end
     change_board # keep playing!
   end
 
-  def keypeg_calc(code_guess) # fxn to change the keypegs from the guess so it prints
-    # compare code_guess to @code then determine the right keypeg sequence
+  def keypeg_calc(code_guess) # fxn to determine the keypegs from the guess so it prints
     code_guess_array = code_guess.to_s.split('').map(&:to_i)
     code_array = @code.to_s.split('').map(&:to_i)
-    puts "code_guess_array is #{code_guess_array}"
-    puts "code_array is #{code_array}"
-    # "!" = correct number and order!
-    # "X" = correct # but wrong order
-    # "O" = miss
-      # NOTE: If there are duplicates in the guess, 
-      # they cannot all be awarded a key peg unless 
-      # they correspond to the same number of duplicates in the code. 
-    
-    # DETERMINE HOW MANY TIMES A NUMBER APPEARS IN THE CODE 
-    # nested array - then create it with a iterating loop
-    
-    # "omits the second optional argument and 
-    # instead passes in the mutable value in a block."
+    # DETERMINE HOW MANY TIMES A NUMBER APPEARS IN THE CODE in a nested array
+    # So I can change elements of the nexted array individually: 
+    # "omits the second optional argument and instead passes in the mutable value in a block."
     code_num_frequency = Array.new(9) {Array.new(2)}
-
-    # add the number to each 1st position
+    # add the number (guess) to each 1st position
     for i in 0..8 do
       code_num_frequency[i][0] = i+1
     end
-    # then add the number to each 2nd position to finish the 
+    # then add the frequency to each 2nd position to finish the 
     # code_num_frequency nested array to regulate the keypegs
     for i in 0..8 do
       code_num_frequency[i][1] = code_array.count(i+1)
     end
-    puts "TEST: code_num_frequency is #{code_num_frequency}" # test
-
-    
-    # establish nested array of number of keypegs to code frequency
+    # If there are duplicates in the guess, 
+    # they cannot all be awarded a key peg unless 
+    # they correspond to the same number of duplicates in the code. 
+    # Nested array of number of keypegs to compare to code_num_frequency
     peg_num_frequency = Array.new(9) {Array.new(2, 0)} # add a zero to each 2nd position
-    # add the number to each 1st position
+    # add the number (guess) to each 1st position
     for i in 0..8 do
       peg_num_frequency[i][0] = i+1
     end
-    
-    # first, need to check all spots first to avoid excluding an exact match "!"
+    # first, check all spots for exact matches to avoid excluding them due to guesses-code duplicates
     keypeg_array = code_guess_array.map.with_index do |guess, index|
       if guess == code_array[index]
-        peg_num_frequency[guess-1][1] = 1 + (peg_num_frequency[guess-1][1]) # marking a direct hit
+        peg_num_frequency[guess-1][1] += 1  # marking a direct hit
         "!"
       else
         "0"
       end
     end
-
-    puts "keypeg_array 1st is #{keypeg_array}"
-    puts "TEST: peg_num_frequency is #{peg_num_frequency}" # test
-
     # second, need to check remaining spots for inexact matches "X"
     keypeg_array = code_guess_array.map.with_index do |guess, index|
       if keypeg_array[index] == "!"
         "!" # make it the same
       elsif code_array.include?(guess) && (peg_num_frequency[guess-1][1]) < (code_num_frequency[guess-1][1]) # there are unclaimed guesses remaining
         # add to the counter
-        peg_num_frequency[guess-1][1] = 1 + (peg_num_frequency[guess-1][1]) # marking an indirect hit
-        # then assign the mark
-        "X"
+        peg_num_frequency[guess-1][1] += 1 # marking an indirect hit
+        "X" # then assign the mark
       else
-        "0"
+        "0" # miss
       end
     end
-
     puts "keypeg_array now is #{keypeg_array}"
     keypeg_array.join("")
   end
-
 
 end # class end
 
@@ -234,22 +212,19 @@ player_name = gets.chomp
 sleep 1
 player = CodeBreaker.new(player_name) # create player object
 puts "Initializing your opponent..."
+sleep 1
 cpu = CodeMaker.new("Tygh's Laptop") # create cpu object
-sleep 1
-puts "OK, it's #{player.name} vs. #{cpu.name}, let's do this!"
 code = cpu.make_code # makes the code!
-sleep 1
-puts "The code is ready..."
+puts "OK, it's #{player.name} vs. #{cpu.name}, let's do this!"
 
 newGame = Mastermind.new(player, cpu, code)
-newGame.show_board
+# newGame.show_board
 newGame.change_board
 
 # - 12 turns to guess the secret code, starting with you guessing the computer’s random code.
 # - Later, refactor your code to allow the human player to choose whether they want to be the creator of the secret code or the guesser.
 
 # Classes
-
 # - Mastermind
 # - Player < codemaker
 # - Player < codebreaker
